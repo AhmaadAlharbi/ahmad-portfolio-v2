4baba3e20c8e0ded87623bbc9e4551787699da6f
AhmadAlharbi <ahmaadzaid7@gmail.com>
2022-11-13 14:41:31 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        Black: "#151515",
+        DarkGrey: "#242424",
+        Green: "#4EE1A0",
+        Grey: "#D9D9D9",
+        White: "#FFFFFF",
+      },
+      fontFamily: {
+        fontSpace: ["Space Grotesk", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
