6c8004e094e47d01f76d54aa8788e2824b541945
ahmadZaid <ahmaadzaid7@gmail.com>
2022-11-15 17:02:13 +0300

    fix form validation

--- app.js
+++ app.js
@@ -6,54 +6,36 @@ const warningText = document.getElementById("warningText");
 const warningIcon = document.getElementById("warningIcon");
 const form = document.querySelector("form");
 const warning = document.querySelector(".warning");
-console.log(warning);
 let counterError = 0;
+
 function validateEmail(email) {
   var re = /\S+@\S+\.\S+/;
   return re.test(email);
 }
+//check if forms inputs empty or not
 
-//check input if it is empty or not
-function checkInput(text) {
-  if (text.value != "") {
-    text.classList.remove("error-border");
-    text.classList.add("border-Green");
-    warning.classList.add("hidden");
-  }
-  if (text.value.length > 0) {
-    if (text.classList.contains("border-Grey")) {
-      text.classList.remove("border-Grey");
-      text.classList.add("border-Green");
-      warning.classList.add("hidden");
-    }
-  } else {
-    text.classList.add("error-border");
+function required(inputtx) {
+  inputtx.classList.remove("error-border");
+  if (inputtx.value.length == 0) {
+    inputtx.classList.add("error-border");
     warning.classList.remove("hidden");
-    counterError++;
+    return false;
   }
+  warning.classList.add("hidden");
+
+  return true;
 }
 
 sendBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  checkInput(inputName);
-  checkInput(message);
-  checkInput(email);
-  if (validateEmail(email.value) == false) {
-    email.classList.add("error-border");
-    counterError++;
-    if (email.value != "") {
+  if (required(inputName) && required(email) && required(message)) {
+    if (validateEmail(email.value) == false) {
+      email.classList.add("error-border");
       warningText.classList.remove("hidden");
       warningIcon.classList.remove("hidden");
+    } else {
+      alert("your email has been sent");
+      form.submit();
     }
-  } else {
-    email.classList.add("border-Green");
-    counterError = 0;
-    warningText.classList.add("hidden");
-    warningIcon.classList.add("hidden");
-  }
-
-  if (counterError == 0) {
-    alert("your email has been sent");
-    form.submit();
   }
 });
