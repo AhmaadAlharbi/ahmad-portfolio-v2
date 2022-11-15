691d8966fedff37a9b7c656d3695779a5b4b60e2
ahmadZaid <ahmaadzaid7@gmail.com>
2022-11-15 17:17:04 +0300

    fix form validation

--- app.js
+++ app.js
@@ -16,20 +16,25 @@ function validateEmail(email) {
 
 function required(inputtx) {
   inputtx.classList.remove("error-border");
+  inputtx.classList.remove("correct-border");
   if (inputtx.value.length == 0) {
     inputtx.classList.add("error-border");
     warning.classList.remove("hidden");
     return false;
   }
   warning.classList.add("hidden");
-
+  inputtx.classList.add("correct-border");
   return true;
 }
 
+//submit the form
 sendBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  //check if they are empty or not
   if (required(inputName) && required(email) && required(message)) {
+    //check if is correct email
     if (validateEmail(email.value) == false) {
+      email.classList.remove("correct-border");
       email.classList.add("error-border");
       warningText.classList.remove("hidden");
       warningIcon.classList.remove("hidden");
